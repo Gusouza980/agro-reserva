77d02d17fe2a337524d3ed2eb5b84007e87091f5
Luis Gustavo <gusouza980@gmail.com>
2021-07-28 17:40:47 -0300

    Update

---
+++ database/migrations/2021_07_25_182737_alter_table_clientes_4.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableClientes4 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('clientes', function (Blueprint $table) {
+            // 0 => Solteiro(a)
+            // 1 => Casado(a)
+            // 2 => Víuvo(a)
+            // 3 => União Estável
+            $table->tinyInteger("estado_civil")->default(0);
+            $table->string("rg", 12)->nullable();
+            $table->string("inscricicao_produtor_rural", 20)->nullable();
+            $table->string("referencia_comercial1", 50)->nullable();
+            $table->string("referencia_comercial1_tel", 20)->nullable();
+            $table->string("referencia_comercial2", 50)->nullable();
+            $table->string("referencia_comercial2_tel", 20)->nullable();
+            $table->string("referencia_comercial3", 50)->nullable();
+            $table->string("referencia_comercial3_tel", 20)->nullable();
+            $table->string("referencia_bancaria_banco")->nullable();
+            $table->string("referencia_bancaria_gerente")->nullable();
+            $table->string("referencia_bancaria_tel, 20")->nullable();
+            $table->string("referencia_coorporativa1")->nullable();
+            $table->string("referencia_coorporativa1_tel", 20)->nullable();
+            $table->string("referencia_coorporativa2")->nullable();
+            $table->string("referencia_coorporativa2_tel", 20)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
