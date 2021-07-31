e16ac4f776d7655b49292af7b48cc27981fa29e3
Luis Gustavo <gusouza980@gmail.com>
2021-07-30 21:36:56 -0300

    Update

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -32,6 +32,7 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     @toastr_css
     @yield("styles")
+    {{--  HOTJAR  --}}
     <!-- Hotjar Tracking Code for dev.agroreserva.com.br -->
     <script>
         (function(h,o,t,j,a,r){
