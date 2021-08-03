27c71d701c1f19428464af8d2ae3519aec4bd0c5
Luis Gustavo <gusouza980@gmail.com>
2021-08-03 16:20:47 -0300

    Update

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -20,36 +20,36 @@
                 <p>Valorizar o trabalho do produtor, ressignificando o formato da comercialização de animais no Brasil, de forma a torná-la mais fácil, justa, segura e rentável para quem compra e para quem vende.</p>  --}}
             </div>
             <div class="row">
-                <div class="d-flex align-items-center text-sobre">
+                <div class="d-flex align-items-center mx-auto mx-lg-0 text-sobre">
                     <div class="px-2 py-2 bg-white" style="border-radius: 15px;">
                         <img src="{{asset('imagens/icone_missao.png')}}" style="width: 80px;" alt="">
                     </div>
                 </div>
-                <div class="text-sobre ml-4">
-                    <h2 class="mt-3">Nossa missão:</h2>
-                    <p>Valorizar o trabalho do produtor, ressignificando o formato da<br>comercialização de animais no Brasil, de forma a torná-la<br>mais fácil, justa, segura e rentável para quem compra e para quem vende.</p>
+                <div class="text-sobre ml-4 text-center text-lg-left">
+                    <h2 class="mt-3 mb-3"><span style="border-bottom: 2px solid #E8521B;">Nos</span>sa missão</h2>
+                    <p>Valorizar o trabalho do produtor, ressignificando o formato da<br class="d-none d-lg-block">comercialização de animais no Brasil, de forma a torná-la<br class="d-none d-lg-block">mais fácil, justa, segura e rentável para quem compra e para quem vende.</p>
                 </div>
             </div>
             <div class="row mt-3">
-                <div class="d-flex align-items-center text-sobre">
+                <div class="d-flex align-items-center mx-auto mx-lg-0 text-sobre">
                     <div class="px-2 py-2 bg-white" style="border-radius: 15px;">
                         <img src="{{asset('imagens/icone_visao.png')}}" style="width: 80px;" alt="">
                     </div>
                 </div>
-                <div class="text-sobre ml-4">
-                    <h2 class="mt-3">Nossa visão:</h2>
-                    <p>Evoluir sempre para nos consolidarmos como referências na<br>comercialização de animais no Brasil e no mundo. </p>
+                <div class="text-sobre ml-4 text-center text-lg-left">
+                    <h2 class="mt-3 mb-3"><span style="border-bottom: 2px solid #E8521B;">Nos</span>sa visão</h2>
+                    <p>Evoluir sempre para nos consolidarmos como referências na<br class="d-none d-lg-block">comercialização de animais no Brasil e no mundo. </p>
                 </div>
             </div>
             <div class="row mt-3">
-                <div class="d-flex align-items-center text-sobre">
+                <div class="d-flex align-items-center mx-auto mx-lg-0 text-sobre">
                     <div class="px-2 py-2 bg-white" style="border-radius: 15px;">
                         <img src="{{asset('imagens/icone_valores.png')}}" style="width: 80px;" alt="">
                     </div>
                 </div>
-                <div class="text-sobre ml-4">
-                    <h2 class="mt-3">Nossos valores:</h2>
-                    <p>Transparência nas relações. Paixão pela pecuária. Precursores<br>da evolução.</p>
+                <div class="text-sobre ml-4 text-center text-lg-left">
+                    <h2 class="mt-3 mb-3"><span style="border-bottom: 2px solid #E8521B;">Nos</span>sos valores</h2>
+                    <p>Transparência nas relações. Paixão pela pecuária. Precursores<br class="d-none d-lg-block">da evolução.</p>
                 </div>
             </div>
         </div>
@@ -58,9 +58,9 @@
                 <div class="col-12 text-sobre-caixa">
                     <h1>Por que comprar ?</h1>
                     <p>
-                        As reservas mais cobiçadas das marcas de melhor reputação do mercado estão aqui.<br>
-                        Os animais valem quanto custam e nada a mais.<br>
-                        Você pode chegar a pagar 0% de comissão.<br>
+                        As reservas mais cobiçadas das marcas de melhor reputação do mercado estão aqui.<br class="d-none d-lg-block">
+                        Os animais valem quanto custam e nada a mais.<br class="d-none d-lg-block">
+                        Você pode chegar a pagar 0% de comissão.<br class="d-none d-lg-block">
                         A gente valoriza sua experiência e acompanha todo o processo de compra com você.
                     </p>
                 </div>
@@ -77,8 +77,8 @@
                 <div class="col-12 text-sobre-caixa">
                     <h1>Por que vender ?</h1>
                     <p>
-                        É um ambiente seguro e transparente.<br>
-                        A gente produz todo o material promocional da sua reserva e da sua fazenda.<br>
+                        É um ambiente seguro e transparente.<br class="d-none d-lg-block">
+                        A gente produz todo o material promocional da sua reserva e da sua fazenda.<br class="d-none d-lg-block">
                         Baixo risco. A gente configura a venda, aprova o crédito e vende pra você.
                     </p>
                 </div>
