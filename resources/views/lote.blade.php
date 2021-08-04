f6806f3fab9e3ac7c18cf523598a9442ad28a1b4
Luis Gustavo <gusouza980@gmail.com>
2021-08-04 10:09:49 -0300

    Update

--- resources/views/lote.blade.php
+++ resources/views/lote.blade.php
@@ -81,19 +81,19 @@
                         <div class="row justify-content-center justify-content-lg-start mt-4 mt-lg-0">
                             <div class="px-4 px-lg-0 text-white flex-grow-1 text-lote-info text-center text-lg-left">
                                 <span><b>RGD:</b> {{$lote->registro}}</span><br>
-                                <hr class="d-md-none" style="border-top: 1px solid white;">
+                                <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
                                 <span><b>CCG:</b> {{$lote->ccg}}</span><br>
-                                <hr class="d-md-none" style="border-top: 1px solid white;">
+                                <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
                                 <span><b>GPTA:</b> {{$lote->gpta}}</span><br>
-                                <hr class="d-md-none" style="border-top: 1px solid white;">
+                                <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
                             </div>
                             <div class="px-4 px-lg-0 ml-lg-4 flex-grow-1 text-white text-lote-info text-center text-lg-left">
                                 <span><b>Raça:</b> {{$lote->raca->nome}}</span><br>
-                                <hr class="d-md-none" style="border-top: 1px solid white;">
+                                <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
                                 <span><b>Nascimento:</b> {{date("d/m/Y", strtotime($lote->nascimento))}}</span><br>
-                                <hr class="d-md-none" style="border-top: 1px solid white;">
+                                <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
                                 <span><b>Último Parto:</b> {{date("d/m/Y", strtotime($lote->parto))}}</span>
-                                <hr class="d-md-none" style="border-top: 1px solid white;">
+                                <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
                             </div>
                         </div>
                         <div class="row justify-content-center justify-content-lg-start mt-2">
