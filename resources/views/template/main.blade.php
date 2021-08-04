e99fd6a09684e55f14be5cd25fd1125ecf60afe4
lgserrania <gusouza980@gmail.com>
2021-08-03 23:36:39 -0300

    Update

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -25,14 +25,13 @@
     @yield("metas")
     <link rel="preload" as="style" href="{{asset('css/main.css')}}" />
     <link rel="preload" as="image" href="{{asset('imagens/bg-home-min-2.jpg')}}" />
-    <link rel="preload" href="{{asset('fontes/gobold/Gobold Regular.otf')}}" as="font" type="font/otf"/>
+    <link rel="preload" href="{{asset('fontes/gobold/Gobold Regular.otf')}}" as="font" type="font/otf" crossorigin/>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 	<link rel="stylesheet" href="{{asset('css/main.css')}}"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/animate.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('css/aos.css')}}"/>
     @toastr_css
     @yield("styles")
     {{--  HOTJAR  --}}
@@ -301,15 +300,11 @@
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js">
+    <script src="{{asset('js/jquery.js')}}">
     </script>
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
+    <script src="{{asset('js/aos.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/5d649ad8-4f69-4811-ab56-9c2bb4d5f5ea-loader.js" ></script>
     @toastr_js
     @toastr_render
