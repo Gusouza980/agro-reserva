dc5af7663d94fb8cfef21ded7bc74001b6770ab7
Luis Gustavo <gusouza980@gmail.com>
2021-08-06 14:44:24 -0300

    Update

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -94,11 +94,13 @@
                 @endif
             </div>
         </div>
-        <div class="row">
-            <div class="col-12 text-right">
-                <a href="{{route('carrinho.checkout')}}"><button class="btn btn-vermelho btn-hover-preto px-3">Continuar</button></a>
+        @if($carrinho->produtos->count() > 0)
+            <div class="row">
+                <div class="col-12 text-right">
+                    <a href="{{route('carrinho.checkout')}}"><button class="btn btn-vermelho btn-hover-preto px-3">Continuar</button></a>
+                </div>
             </div>
-        </div>
+        @endif
         <hr>
     </div>
 @endsection
