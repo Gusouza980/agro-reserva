3b7272ea4ed1c2f253ff6937f01f2595ee40f8ab
Luis Gustavo <gusouza980@gmail.com>
2021-08-16 16:03:44 -0300

    Update

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -80,7 +80,7 @@ class ContaController extends Controller
 
     public function comprovante_reserva(Venda $venda){
         $data = ["venda" => $venda];
-        $pdf = PDF::loadView('cliente.comprovante2', $data);
+        $pdf = PDF::loadView('cliente.comprovante', $data);
         return $pdf->stream();
     }
 }
