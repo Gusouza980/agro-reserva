963a44dcceb6a62096f8f8f23912a79efe08e6cb
Luis Gustavo <gusouza980@gmail.com>
2021-09-23 18:01:26 -0300

    Update

--- resources/views/includes/lote/modelo02.blade.php
+++ resources/views/includes/lote/modelo02.blade.php
@@ -1,18 +1,24 @@
+{{-- MODELO DE PRENHEZ --}}
 <div class="row justify-content-center justify-content-lg-start mt-4 mt-lg-0">
     <div class="px-4 px-lg-0 text-white flex-grow-1 text-lote-info text-center text-lg-left">
-        <span><b>RGD:</b> {{$lote->registro}}</span><br>
-        <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
-        <span><b>Raça:</b> {{$lote->raca->nome}}</span><br>
+        @if ($lote->registro)
+            <span><b>RGD:</b> {{ $lote->registro }}</span><br>
+            <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
+        @endif
+        <span><b>Raça:</b> {{ $lote->raca->nome }}</span><br>
         <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
     </div>
     <div class="px-4 px-lg-0 ml-lg-4 flex-grow-1 text-white text-lote-info text-center text-lg-left">
-        @if($lote->nascimento)
-            <span><b>Prev. Prenhez:</b> {{date("m/Y", strtotime($lote->nascimento))}}</span><br>
+        @if ($lote->nascimento)
+            <span><b>Prev. Prenhez:</b> {{ date('m/Y', strtotime($lote->nascimento)) }}</span><br>
+            <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
         @else
             <span><b>Prev. Prenhez:</b> PRENHEZ A FAZER</span><br>
+            <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
         @endif
-        <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
-        <span><b>Sexo da Prenhez:</b> {{$lote->sexo}}</span><br>
-        <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
+        @if ($lote->sexo)
+            <span><b>Sexo da Prenhez:</b> {{ $lote->sexo }}</span><br>
+            <hr class="d-md-none" style="border-top: 1px solid rgba(255,255,255,0.4);">
+        @endif
     </div>
 </div>
