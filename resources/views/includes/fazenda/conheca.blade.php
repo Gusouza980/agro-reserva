9c82c3ff50b888b9ebe52d55f3c34befbcfbe277
Luis Gustavo <gusouza980@gmail.com>
2021-10-11 20:02:26 -0300

    update

--- resources/views/includes/fazenda/conheca.blade.php
+++ resources/views/includes/fazenda/conheca.blade.php
@@ -18,13 +18,20 @@
                             <span>{{$fazenda->texto_conheca}}</span>
                         </div>
                     </div>
+                    <div class="row mt-2 pb-4">
+                        <div class="d-none d-md-block col-12 px-0 text-section2-fazenda text-center text-md-left">
+                            <a name="" id="" class="btn btn-vermelho py-2 px-4 mt-3" href="{{route('fazenda.lotes', ['fazenda' => $fazenda->slug])}}" role="button">Ver animais à venda</a>
+                            <br class="d-lg-none">
+                            <a  href="https://api.whatsapp.com/send?phone=5514981809051" target="_blank" class="btn btn-vermelho ml-lg-4 px-4 py-2 mt-3">Falar com Consultor</a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-12 col-lg-7 video-container video-container-depoimento mb-5 mb-lg-0">
                 {{--  <iframe src="{{$fazenda->video_conheca}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  --}}
                 {!! $fazenda->video_conheca !!}
             </div>
-            <div class="col-12 px-0 text-section2-fazenda text-center text-md-left pb-4">
+            <div class="d-md-none col-12 px-0 text-section2-fazenda text-center text-md-left pb-4">
                 <a name="" id="" class="btn btn-vermelho py-2 px-4 mt-3" href="{{route('fazenda.lotes', ['fazenda' => $fazenda->slug])}}" role="button">Ver animais à venda</a>
                 <br class="d-lg-none">
                 <a  href="https://api.whatsapp.com/send?phone=5514981809051" target="_blank" class="btn btn-vermelho ml-lg-4 px-4 py-2 mt-3">Falar com Consultor</a>
