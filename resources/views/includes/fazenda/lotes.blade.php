3f477218357e1d3ac0d48a21c5ed24f0b6035e39
Luis Gustavo <gusouza980@gmail.com>
2021-10-11 20:00:06 -0300

    Update

--- resources/views/includes/fazenda/lotes.blade.php
+++ resources/views/includes/fazenda/lotes.blade.php
@@ -21,17 +21,15 @@
                             </div>
                         @endforeach
                     </div>
-                    <div class="row py-4">
-                        <div class="col-12 px-0 text-section2-fazenda text-center text-md-left">
-                            <a name="" id="" class="btn btn-vermelho py-2 px-4 mt-3" href="{{route('fazenda.lotes', ['fazenda' => $fazenda->slug])}}" role="button">Ver animais à venda</a>
-                            <br class="d-lg-none">
-                            <a  href="https://api.whatsapp.com/send?phone=5514981809051" target="_blank" class="btn btn-vermelho ml-lg-4 px-4 py-2 mt-3">Falar com Consultor</a>
-                        </div>
-                    </div>
                 </div>
             </div>
             <div class="col-12 col-lg-7 video-container video-container-depoimento mb-5 mb-lg-0 px-0">
                 {!! $fazenda->video_conheca_lotes !!}
+            </div>
+            <div class="col-12 px-0 text-section2-fazenda text-center text-md-left pb-4">
+                <a name="" id="" class="btn btn-vermelho py-2 px-4 mt-3" href="{{route('fazenda.lotes', ['fazenda' => $fazenda->slug])}}" role="button">Ver animais à venda</a>
+                <br class="d-lg-none">
+                <a  href="https://api.whatsapp.com/send?phone=5514981809051" target="_blank" class="btn btn-vermelho ml-lg-4 px-4 py-2 mt-3">Falar com Consultor</a>
             </div>
         </div>
         
