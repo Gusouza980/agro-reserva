0906ca1c3f13e7a0a60d5596c0ed8e8e0473e7c8
Luis Gustavo <gusouza980@gmail.com>
2021-10-12 13:13:06 -0300

    Update

--- resources/views/includes/fazenda/conheca.blade.php
+++ resources/views/includes/fazenda/conheca.blade.php
@@ -34,7 +34,7 @@
             <div class="d-md-none col-12 px-0 text-section2-fazenda text-center text-md-left pb-4">
                 <a name="" id="" class="btn btn-vermelho py-2 px-4 mt-3" href="{{route('fazenda.lotes', ['fazenda' => $fazenda->slug])}}" role="button">Ver animais à venda</a>
                 <br class="d-lg-none">
-                <a  href="https://api.whatsapp.com/send?phone=5514981809051" target="_blank" class="btn btn-vermelho ml-lg-4 px-4 py-2 mt-3">Falar com Consultor</a>
+                <a  href="https://api.whatsapp.com/send?phone={{$reserva->telefone_consultor}}" target="_blank" class="btn btn-vermelho ml-lg-4 px-4 py-2 mt-3">Falar com Consultor</a>
             </div>
         </div>
         
