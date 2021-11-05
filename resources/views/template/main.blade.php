79edc9c461aa04f264ff59180c7bae3e9241caf7
Luis Gustavo <gusouza980@gmail.com>
2021-11-05 17:17:59 -0300

    Update

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -30,14 +30,14 @@
     <meta property="og:updated_time" content="1440432930" />
     <meta name="facebook-domain-verification" content="abk9vvukcc2zjrkcwtq8qncjjfb68f" />
     @yield("metas")
-    <link rel="preload" as="style" href="{{ asset('css/main.css') }}?v=1.3.7" />
+    <link rel="preload" as="style" href="{{ asset('css/main.css') }}?v=1.3.8" />
     <link rel="preload" as="image" href="{{ asset('imagens/bg-home-min-2.jpg') }}" />
     <link rel="preload" href="{{ asset('fontes/gobold/Gobold Regular.otf') }}" as="font" type="font/otf"
         crossorigin />
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}?v=1.3.7" />
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}?v=1.3.8" />
     <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/aos.css') }}" />
     @toastr_css
