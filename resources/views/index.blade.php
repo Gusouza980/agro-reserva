a9d9dd25d9c6ab8dd650602316467935f32cbbec
Luis Gustavo <gusouza980@gmail.com>
2021-11-17 18:13:44 -0300

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -49,11 +49,11 @@
                             style="position: absolute; bottom:0; left:0; width: 100%;background: rgb(0,0,0); background: linear-gradient(0deg, rgba(0,0,0,1) 25%, rgba(0,212,255,0) 100%); height: 250px;">
                         </div> --}}
                         <div id="banner-arrow-left"
-                            style="position: absolute; left: 30px; top: calc(50% - 20px); padding: 7px 10px 5px 10px; background-color: rgba(0,0,0, 0.5); border-radius: 5px; cursor: pointer;">
+                            style="position: absolute; left: 30px; top: calc(50% - 60px); padding: 7px 10px 5px 10px; background-color: rgba(0,0,0, 0.8); border-radius: 5px; cursor: pointer;">
                             <i class="fas fa-chevron-left text-white fa-lg"></i>
                         </div>
                         <div id="banner-arrow-right"
-                            style="position: absolute; right: 30px; top: calc(50% - 20px); padding: 7px 10px 5px 10px; background-color: rgba(0,0,0, 0.5); border-radius: 5px; cursor: pointer;">
+                            style="position: absolute; right: 30px; top: calc(50% - 60px); padding: 7px 10px 5px 10px; background-color: rgba(0,0,0, 0.8); border-radius: 5px; cursor: pointer;">
                             <i class="fas fa-chevron-right text-white fa-lg"></i>
                         </div>
                     </div>
