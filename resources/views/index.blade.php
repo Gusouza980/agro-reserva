3d478f4f46a325b138693cd38d4fce072d9a5fee
Luis Gustavo <gusouza980@gmail.com>
2021-12-02 14:26:51 -0300

    Update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,7 +95,7 @@
                             </div>
                         </div>
                     @endif
-                    @if(false)
+                    @if(true)
                         <div class="col-12 text-center text-header-index d-none d-lg-block">
                             <h5>Vitrine de animais</h5>
                         </div>
