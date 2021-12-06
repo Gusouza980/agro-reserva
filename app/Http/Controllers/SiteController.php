350c61df00ac8baf1b76c26ce55c2b0fd319f7dd
Luis Gustavo <gusouza980@gmail.com>
2021-12-06 10:00:00 -0300

    Update

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -69,7 +69,13 @@ class SiteController extends Controller
         // dd($cliente->toArray());
         $configuracao = Configuracao::first();
         $reservas = Reserva::where("ativo", true)->orderBy("inicio", "ASC")->get();
-        return view("index", ["reservas" => $reservas, "configuracao" => $configuracao]);
+        $lotes = Reserva::where([["aberto", true], ['encerrada', false]])->first()->lotes;
+        $lotes_destaque = $lotes->where("reservado", false)->sortByDesc("visualizacoes");
+        if($lotes_destaque->count() < 10){
+            $count = 10 - $lotes_destaque->count();
+            $lotes_destaque = $lotes_destaque->merge($lotes->where("reservado", true)->sortByDesc("visualizacoes")->take($count));
+        }
+        return view("index", ["reservas" => $reservas, "configuracao" => $configuracao, "lotes_destaque" => $lotes_destaque]);
         // $beneficiario = new \Eduardokum\LaravelBoleto\Pessoa(
         //     [
         //         'nome'      => 'Agroreserva',
