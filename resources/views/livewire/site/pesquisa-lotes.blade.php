dde22a2134754c6ce012b445490fea2ed00b1c25
Luis Gustavo <gusouza980@gmail.com>
2022-03-22 19:59:42 -0300

    Update

--- resources/views/livewire/site/pesquisa-lotes.blade.php
+++ resources/views/livewire/site/pesquisa-lotes.blade.php
@@ -16,14 +16,14 @@
     
     <div class="row" style="">
         @if($resultados)
-            <table class="table">
+            <table class="table table-hover">
                 <tbody>
                     @foreach($resultados as $lote)
-                        <tr class="tr-pesquisa" wire:key='{{ $loop->index }}' onclick="window.location.href='{{ route('fazenda.lote', ['fazenda' => $lote->fazenda->slug, 'lote' => $lote, 'reserva' => $lote->reserva]) }}'">
+                        <tr class="tr-pesquisa cpointer" wire:key='{{ $loop->index }}' onclick="window.location.href='{{ route('fazenda.lote', ['fazenda' => $lote->fazenda->slug, 'lote' => $lote, 'reserva' => $lote->reserva]) }}'">
                             <td style="vertical-align: middle;" scope="row" wire:key='{{ $loop->index }}'>{{ $lote->nome }}</td>
                             <td style="vertical-align: middle;" wire:key='{{ $loop->index }}'>{{ $lote->fazenda->nome_fazenda }}</td>
                             <td style="vertical-align: middle;" wire:key='{{ $loop->index }}'>R${{ number_format($lote->preco, 2, ",", ".") }}</td>
-                            <td style="vertical-align: middle;" wire:key='{{ $loop->index }}'><a name="" id="" class="btn btn-warning d-none d-md-block" href="#" role="button"><i class="fas fa-plus" style="font-size: 10px;"></i></a></td>
+                            {{-- <td style="vertical-align: middle;" wire:key='{{ $loop->index }}'><a name="" id="" class="btn btn-warning d-none d-md-block" href="#" role="button"><i class="fas fa-plus" style="font-size: 10px;"></i></a></td> --}}
                         </tr>
                     @endforeach
                 </tbody>
