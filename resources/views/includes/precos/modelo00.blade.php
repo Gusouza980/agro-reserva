30e5bf3d0cda42d85cca1d3243f5ac92deb46fff
Luis Gustavo <gusouza980@gmail.com>
2022-03-22 20:06:26 -0300

    Update

--- resources/views/includes/precos/modelo00.blade.php
+++ resources/views/includes/precos/modelo00.blade.php
@@ -2,31 +2,31 @@
 <div class="row">
     <div class="col-12 text-center text-lg-right @if (!$lote->reserva->preco_disponivel && !$lote->liberar_preco) blur @endif">
         @if ($lote->reserva->preco_disponivel || $lote->liberar_preco)
-            @if ($lote->reserva->parcelas_mes == 1)
-                <h4><b>{{ $lote->parcelas }}x</b> de
-                    <b>R${{ number_format($lote->preco / $lote->parcelas, 2, ',', '.') }}</b>
-                </h4>
-            @else
-                <div>
-                    <h4><b>30</b>x (15 duplas) de
-                        <b>R${{ number_format($lote->preco / ($lote->parcelas), 2, ',', '.') }}</b>
-                    </h4>
-                </div>
-            @endif
+            <h4>ou R${{ number_format($lote->preco - ($lote->preco * $lote->reserva->desconto) / 100, 2, ',', '.') }}
+                à
+                vista</h4>
         @else
-            <h4><b>0x</b> de <b>R$0000,00</b></h4>
+            <h4>R$00000,00</h4>
         @endif
+        {{-- <span>R$00000,00</span> --}}
     </div>
 </div>
 <div class="row">
     <div class="col-12 text-center text-lg-right @if (!$lote->reserva->preco_disponivel && !$lote->liberar_preco) blur @endif">
         @if ($lote->reserva->preco_disponivel || $lote->liberar_preco)
-            <span>ou R${{ number_format($lote->preco - ($lote->preco * $lote->reserva->desconto) / 100, 2, ',', '.') }}
-                à
-                vista</span>
+            @if ($lote->reserva->parcelas_mes == 1)
+                <span><b>{{ $lote->parcelas }}x</b> de
+                    <b>R${{ number_format($lote->preco / $lote->parcelas, 2, ',', '.') }}</b>
+                </span>
+            @else
+                <div>
+                    <span><b>30</b>x (15 duplas) de
+                        <b>R${{ number_format($lote->preco / ($lote->parcelas), 2, ',', '.') }}</b>
+                    </span>
+                </div>
+            @endif
         @else
-            <span>R$00000,00</span>
+            <span><b>0x</b> de <b>R$0000,00</b></span>
         @endif
-        {{-- <span>R$00000,00</span> --}}
     </div>
 </div>
