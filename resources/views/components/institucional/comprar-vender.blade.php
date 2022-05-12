0e6a53e4a390b0b07bf3dad1ef07fb5ec9f68ec5
Luis Gustavo <gusouza980@gmail.com>
2022-05-12 15:26:34 -0300

    Update

--- resources/views/components/institucional/comprar-vender.blade.php
+++ resources/views/components/institucional/comprar-vender.blade.php
@@ -2,21 +2,21 @@
     <div class="w1400 mx-auto px-3 px-lg-0" x-show="show" x-transition.opacity.duration.3000ms>
         <div class="row">
             <div class="col-12 col-lg-6 mb-4">
-                <div class="shadow-sm">
+                <div class="shadow-sm bg-branco hover:scale-105 hover:bg-orange-400 hover:text-white cpointer">
                     <div class="rounded-t-md">
                         <img src="{{ asset('imagens/banner-experiencia-berrante.jpg') }}" style="max-width: 100%;" alt="">
                     </div>
-                    <div class="text-center py-2 bg-white rounded-b-md" style="width: 100%; font-family: Montserrat; font-size: 18px;">
+                    <div class="text-center py-2 bg-inherit rounded-b-md cpointer text-inherit" style="width: 100%; font-family: Montserrat; font-size: 18px;">
                         Quero Comprar
                     </div>
                 </div>
             </div>
             <div class="col-12 col-lg-6 mb-4">
-                <div class="shadow-sm">
+                <div class="shadow-sm bg-branco hover:scale-105 hover:bg-orange-400 hover:text-white cpointer">
                     <div class="rounded-t-md">
                         <img src="{{ asset('imagens/banner-experiencia-berrante.jpg') }}" style="max-width: 100%;" alt="">
                     </div>
-                    <div class="text-center py-2 bg-white rounded-b-md" style="width: 100%; font-family: Montserrat; font-size: 18px;">
+                    <div class="text-center py-2 bg-inherit rounded-b-md cpointer text-inherit" style="width: 100%; font-family: Montserrat; font-size: 18px;">
                         Quero Vender
                     </div>
                 </div>
