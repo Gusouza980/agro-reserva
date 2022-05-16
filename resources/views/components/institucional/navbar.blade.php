4988ab6f554fd7ca208987804cf83d8d16cc1ff4
Luis Gustavo <gusouza980@gmail.com>
2022-05-16 11:28:51 -0300

    Update

--- resources/views/components/institucional/navbar.blade.php
+++ resources/views/components/institucional/navbar.blade.php
@@ -112,10 +112,26 @@
             </div>
         @else
             <div class="flex items-center md:order-2">
-                <a class="flex align-items-center mr-3 text-sm rounded-full md:mr-0 cpointer"
-                    id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
-                    <span class="text-white ml-2">Entrar/Cadastrar <i class="fas fa-chevron-down fa-sm text-white ml-2"></i> </span>
-                </a>
+                <div class="mr-3 text-center cpointer transition duration-500 hover:scale-105" style="font-size: 10px;" aria-expanded="false" type="button" data-dropdown-toggle="dropdown">
+                    <svg class="mx-auto" id="Grupo_3722" data-name="Grupo 3722" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="37.018" height="37" viewBox="0 0 37.018 37">
+                        <defs>
+                          <clipPath id="clip-path">
+                            <rect id="Retângulo_2481" data-name="Retângulo 2481" width="37.018" height="37" fill="#80828b"/>
+                          </clipPath>
+                        </defs>
+                        <g id="Grupo_3721" data-name="Grupo 3721" clip-path="url(#clip-path)">
+                          <path id="Caminho_2314" data-name="Caminho 2314" d="M12.595,147.743c.054.051.109.1.164.151a9.254,9.254,0,0,0,3.335,2l.051.015c.186.06.376.113.567.159.074.018.148.037.223.053.124.027.25.047.377.067.095.016.188.038.283.051L15.92,154l-4.137-5.689.276-1.107c.032.034.065.064.1.1C12.3,147.456,12.445,147.6,12.595,147.743ZM9.881,137.351c.3.046.626.08.938.121l.344.044c.449.056.9.107,1.363.151q.534.053,1.076.1c.264.02.526.041.793.058q.847.056,1.7.089l.346.012c.69.023,1.382.036,2.071.036s1.38-.013,2.071-.036l.347-.012q.855-.033,1.7-.089c.267-.017.53-.038.793-.058q.543-.043,1.076-.1c.463-.044.915-.1,1.364-.151l.345-.044c.312-.041.634-.075.939-.121v1.213a11.29,11.29,0,0,1-3.529,8.246,7.747,7.747,0,0,1-5.108,2.242,7.579,7.579,0,0,1-4.773-1.945,10.987,10.987,0,0,1-3.046-4.28,11.467,11.467,0,0,1-.818-4.264Zm-8.4-4.33H35.558c-.862,1.168-4.059,2.468-9.037,3.174-.145.02-.294.038-.442.057-.242.032-.488.064-.739.092s-.479.054-.721.078c-.169.017-.342.033-.515.049-.273.025-.547.051-.824.072l-.277.018q-.77.057-1.557.091c-.285.013-.57.025-.861.034-.187.006-.375.012-.563.016-.493.011-.992.02-1.5.02s-1.011-.009-1.5-.02c-.188,0-.376-.01-.563-.016-.291-.009-.577-.021-.861-.034q-.787-.036-1.557-.091l-.276-.018c-.278-.022-.551-.047-.825-.072-.172-.015-.345-.031-.514-.049q-.364-.037-.721-.078c-.251-.028-.5-.06-.739-.092-.147-.019-.3-.037-.442-.057-4.978-.707-8.175-2.006-9.037-3.174Zm23.5,14.187.277,1.107L21.116,154l-1.674-3.766c.095-.012.189-.035.284-.051.126-.02.253-.04.376-.067.075-.016.149-.035.223-.053q.287-.069.567-.159l.051-.015a9.253,9.253,0,0,0,3.335-2c.055-.05.11-.1.164-.151.149-.142.3-.287.438-.437C24.911,147.271,24.945,147.242,24.977,147.208Zm.969-1.21a13.185,13.185,0,0,0,2.444-7.434v-1.426c4.947-.89,8.638-2.491,8.638-4.733a.617.617,0,0,0-.617-.617H27.643l-.292-1.234H9.686l-.293,1.234H.627a.616.616,0,0,0-.617.617c0,2.243,3.689,3.843,8.638,4.733v1.426A13.185,13.185,0,0,0,11.09,146l-.445,1.781c-.245.116-.5.236-.764.363v10.781H27.156V148.142c-.266-.128-.519-.247-.765-.363Z" transform="translate(-0.009 -121.923)" fill="#80828b" fill-rule="evenodd"/>
+                          <path id="Caminho_2315" data-name="Caminho 2315" d="M167.6,93.218H151.11l-.292,1.234H167.9Z" transform="translate(-140.849 -87.056)" fill="#80828b" fill-rule="evenodd"/>
+                          <path id="Caminho_2316" data-name="Caminho 2316" d="M174.814,1.78A2.35,2.35,0,0,0,171.861.1a14.871,14.871,0,0,1-8.509,0A2.35,2.35,0,0,0,160.4,1.788l-.745,3.14H175.56Z" transform="translate(-149.098 0)" fill="#80828b" fill-rule="evenodd"/>
+                          <path id="Caminho_2317" data-name="Caminho 2317" d="M429.33,415.969h1.851v-9.225c-.647-.344-1.262-.658-1.851-.952Z" transform="translate(-400.95 -378.969)" fill="#80828b" fill-rule="evenodd"/>
+                          <path id="Caminho_2318" data-name="Caminho 2318" d="M0,435.833V438.3a.617.617,0,0,0,.617.617H5.553v-8.552c-1.219.681-2.5,1.445-3.773,2.29A3.946,3.946,0,0,0,0,435.833" transform="translate(0 -401.918)" fill="#80828b" fill-rule="evenodd"/>
+                          <path id="Caminho_2319" data-name="Caminho 2319" d="M102.67,415.964h1.851V405.788c-.589.293-1.2.607-1.851.952Z" transform="translate(-95.883 -378.965)" fill="#80828b" fill-rule="evenodd"/>
+                          <path id="Caminho_2320" data-name="Caminho 2320" d="M479.769,432.66c-1.276-.845-2.553-1.609-3.773-2.29v8.552h4.936a.617.617,0,0,0,.617-.617v-2.468a3.947,3.947,0,0,0-1.78-3.177" transform="translate(-444.532 -401.922)" fill="#80828b" fill-rule="evenodd"/>
+                        </g>
+                    </svg>
+                    Minha Conta
+                </div>
+                
                 <!-- Dropdown menu -->
                 <div class="hidden z-50 my-4 text-base list-none bg-preto rounded divide-y divide-gray-100 shadow dark:divide-gray-600"
                     id="dropdown">
@@ -125,7 +141,7 @@
                                 class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Entrar</a>
                         </li>
                         <li>
-                            <a href="{{ route('carrinho') }}"
+                            <a href="{{ route('cadastro') }}"
                                 class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Cadastrar</a>
                         </li>
                     </ul>
