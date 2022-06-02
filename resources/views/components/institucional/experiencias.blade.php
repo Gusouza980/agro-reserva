0c1453e226da285a1cc11b9943de00f081ab2960
Luis Gustavo <gusouza980@gmail.com>
2022-06-02 00:15:51 -0300

    Update

--- resources/views/components/institucional/experiencias.blade.php
+++ resources/views/components/institucional/experiencias.blade.php
@@ -1,27 +1,27 @@
-<div class="container-fluid px-0 bg-white py-5" x-data="{ show: false }" x-intersect.enter="show = true">
-    <div class="w1400 mx-auto px-3 px-lg-0" x-show="show" x-transition.opacity.duration.3000ms>
-        <div class="row mb-5">
-            <div class="col-12 text-center" style="font-family: Montserrat; font-size: 25px; font-weight: medium;">
+<div class="px-0 py-5 bg-white container-fluid" x-data="{ show: false }" x-intersect.enter="show = true">
+    <div class="px-3 mx-auto w1400 px-lg-0" x-show="show" x-transition.opacity.duration.3000ms>
+        <div class="mb-5 row">
+            <div class="text-center col-12" style="font-family: Montserrat; font-size: 25px; font-weight: medium;">
                 O GRUPO AGRO EXPERIENCE
             </div>
         </div>
         <div class="row">
-            <div class="col-12 col-lg-6 mb-4 animate-in slide-in-from-left delay-300 duration-1000">
-                <div class="shadow-sm bg-branco transition duration-500 hover:scale-105 hover:bg-black hover:text-white cpointer">
-                    <div class="rounded-t-md">
+            <div class="mb-4 duration-1000 delay-300 col-12 col-lg-6 animate-in slide-in-from-left">
+                <div class="rounded-md overflow-hidden shadow-sm bg-branco transition duration-500 hover:scale-105 hover:bg-[#E8521B] hover:text-white cpointer">
+                    <div class="">
                         <img src="{{ asset('imagens/banner-experiencia-berrante.jpg') }}" style="max-width: 100%;" alt="">
                     </div>
-                    <div class="text-center py-2 bg-inherit rounded-b-md cpointer text-inherit" style="width: 100%; font-family: Montserrat; font-size: 18px;">
+                    <div class="py-2 text-center bg-inherit rounded-b-md cpointer text-inherit" style="width: 100%; font-family: Montserrat; font-size: 18px;">
                         BERRANTE - AGRO EXPERIENCE
                     </div>
                 </div>
             </div>
-            <div class="col-12 col-lg-6 mb-4 animate-in slide-in-from-right delay-300 duration-1000">
-                <div class="shadow-sm bg-branco transition duration-500 hover:scale-105 hover:bg-black hover:text-white cpointer" onclick="window.location.href = '{{ route('experiencias.ouro_branco') }}'">
-                    <div class="rounded-t-md">
+            <div class="mb-4 duration-1000 delay-300 col-12 col-lg-6 animate-in slide-in-from-right">
+                <div class="rounded-md overflow-hidden shadow-sm bg-branco transition duration-500 hover:scale-105 hover:bg-[#E8521B] hover:text-white cpointer" onclick="window.location.href = '{{ route('experiencias.ouro_branco') }}'">
+                    <div class="">
                         <img src="{{ asset('imagens/banner-ourobranco.jpg') }}" style="max-width: 100%;" alt="">
                     </div>
-                    <div class="text-center py-2 bg-inherit rounded-b-md cpointer text-inherit" style="width: 100%; font-family: Montserrat; font-size: 18px;">
+                    <div class="py-2 text-center bg-inherit rounded-b-md cpointer text-inherit" style="width: 100%; font-family: Montserrat; font-size: 18px;">
                         OURO BRANCO
                     </div>
                 </div>
--- resources/views/experiencias/ouro-branco.blade.php
+++ resources/views/experiencias/ouro-branco.blade.php
@@ -6,26 +6,11 @@
 <div class="w-full">
     <img src="{{ asset('imagens/banner-pagina-ourobranco.png') }}" class="w-full" alt="">
 </div>
-<div class="flex justify-content-center py-4">
+<div class="flex py-4 justify-content-center">
     <div class="w-full lg:w-1/2">
-        <iframe class="aspect-video w-full" src="https://www.youtube.com/embed/s4nvrH6Xz60?list=PLnqvSti-aWALfE84ooJzeFQlW_o45kuPB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe class="w-full overflow-hidden rounded-md aspect-video" src="https://www.youtube.com/embed/s4nvrH6Xz60?list=PLnqvSti-aWALfE84ooJzeFQlW_o45kuPB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </div>
-<div class="w1400 mx-auto mt-4">
-    <div class="flex flex-wrap lg:-mx-2 justify-content-center">
-        @foreach($videos as $video)
-            @php
-                // dd($video->contentDetails->videoId);
-            @endphp
-            <div class="w-full lg:w-1/3 px-2 mb-3">
-                @livewire('experiencias.video', ['video' => $video])
-                <div class="" style="font-size: 16px; font-family: Montserrat;">
-                    {{ $video->snippet->title }}
-                </div>
-                {{-- <img src="{{ $video->snippet->thumbnails->medium->url }}" class="w-full" alt=""> --}}
-            </div>
-        @endforeach
-    </div>
-</div>
+@livewire('experiencias.ouro-branco')
 
 @endsection
