3e7cf260bac3419d37ddb775050a9189880c718f
Luis Gustavo <gusouza980@gmail.com>
2022-08-17 15:19:48 -0300

    Update

--- resources/views/components/institucional/slide-lotes-destaque.blade.php
+++ resources/views/components/institucional/slide-lotes-destaque.blade.php
@@ -9,9 +9,15 @@
                             <div class="text-center justify-content-center align-items-center lote-home-hover">
                                 <p style="margin-top: 12px;">{{ __('messages.botoes.compre_agora') }}</p>
                             </div>
-                            <div class="@if(!$lote->reservado) hidden @endif font-montserrat text-[29px] text-[#FFB02A] font-bold absolute top-0 left-0 z-[10] w-full h-full rounded-t-[15px] flex items-center justify-center" style="background-color: rgba(0,0,0,0.45)">
-                                VENDIDO
-                            </div>
+                            @if($lote->reservado || $lote->reserva->encerrada)
+                                <div class="font-montserrat text-[29px] text-[#FFB02A] font-bold absolute top-0 left-0 z-[10] w-full h-full rounded-t-[15px] flex items-center justify-center" style="background-color: rgba(0,0,0,0.45)">
+                                    @if($lote->reserva->encerrada)
+                                        ENCERRADO
+                                    @else
+                                        VENDIDO
+                                    @endif
+                                </div>
+                            @endif
                         </div>
                         <div class="px-4 mt-3 row align-items-center justify-content-start">
                             <div class="caixa-lote-home-logo d-flex align-items-center justify-content-center">
