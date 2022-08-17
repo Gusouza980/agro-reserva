9b2fa6ff745a57245e04d0139e784c4f4fe68e04
Luis Gustavo <gusouza980@gmail.com>
2022-08-17 16:39:39 -0300

    Update

--- resources/views/livewire/institucional/lotes/detalhes.blade.php
+++ resources/views/livewire/institucional/lotes/detalhes.blade.php
@@ -72,7 +72,7 @@
                     <div class="w-full font-montserrat text-[14px]">
                         <span>Sem juros no boleto de titularidade Faz. e comprador.</span>
                     </div>
-                    @if(!$lote->reservado && !$lote->reserva->encerrada)
+                    @if($lote->reservado || $lote->reserva->encerrada || !$lote->reserva->compra_disponivel)
                         <div class="w-full mt-[20px]">
                             <a onclick="Livewire.emit('adicionarProduto', {{ $lote->produto->id }})"
                                 class="cpointer bg-[#14C656] text-white font-montserrat text-[18px] font-medium py-[12px] px-[60px] rounded-[15px]">Comprar</a>
