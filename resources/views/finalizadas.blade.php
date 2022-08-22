3b80afb398d01a5b51a1419c6d49854a0c2d8ae9
Luis Gustavo <gusouza980@gmail.com>
2022-08-22 16:18:53 -0300

    Update

--- resources/views/finalizadas.blade.php
+++ resources/views/finalizadas.blade.php
@@ -5,7 +5,7 @@
 <div class="w-full">
     <div class="grid grid-cols-4 mx-auto w1200 hide-scroll-bar gap-x-5 gap-y-10 my-20">
         @foreach($reservas->sortByDesc("fim") as $reserva)
-            <div class="w-full" style="border-radius: 15px; position: relative; height: auto;">
+            <div class="w-full" style="border-radius: 15px; position: relative; height: auto; background: black;">
                 <img src="{{ asset($reserva->imagem_card) }}" class="w-full" alt="">
                 <div class="d-flex align-items-center justify-content-center w-full" style="position: absolute; bottom: 0px; width: 100%; height: 100px; left: 0px;">
                     <div class="text-center">
