c39522ed1f7a3928223ee10b0ecc8d0f893143f1
Luis Gustavo <gusouza980@gmail.com>
2022-10-31 10:43:27 -0300

    Update

--- resources/views/includes/tags/google-ads.blade.php
+++ resources/views/includes/tags/google-ads.blade.php
@@ -1,9 +1,9 @@
-<!-- Global site tag (gtag.js) - Google Ads: 343048869 -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-343048869"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-246416035-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'AW-343048869');
+  gtag('config', 'UA-246416035-1');
 </script>
