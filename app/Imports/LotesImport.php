c6a073fa545e42230c81344e8f33595da2e253f8
Luis Gustavo de Souza Carvalho <gusouza980@gmail.com>
2022-11-01 16:17:22 -0300

    Update

--- app/Imports/LotesImport.php
+++ app/Imports/LotesImport.php
@@ -42,6 +42,10 @@ class LotesImport implements ToModel, WithHeadingRow, WithCalculatedFormulas
             $lote->nascimento = Util::convertDateToString($lote->nascimento);
         }
 
+        if($lote->numero == null){
+            return null;
+        }
+
         return $lote;
     }
 }
