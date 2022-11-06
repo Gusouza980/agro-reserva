fe42e6d7767032c7ec0d32a1cdf1a499553ab52f
Luis Gustavo <gusouza980@gmail.com>
2022-11-06 09:01:57 -0300

    Update

--- app/Http/Livewire/Painel/Lotes/Datatable.php
+++ app/Http/Livewire/Painel/Lotes/Datatable.php
@@ -44,7 +44,7 @@ class Datatable extends Component
 
     public function atualizaValor(Lote $lote, $campo, $valor){
         $lote->$campo = $valor;
-        if($campo == "preco" && $valor !== null){
+        if($campo == "preco" && !empty($valor)){
             $lote->produto->preco = $valor;
             $lote->produto->save();
         }
