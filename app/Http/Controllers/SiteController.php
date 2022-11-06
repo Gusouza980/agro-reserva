286c20a238b8b1400a39f830677bd3f727d929a5
Luis Gustavo <gusouza980@gmail.com>
2022-11-06 09:47:56 -0300

    Update

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -35,8 +35,9 @@ class SiteController extends Controller
         // return view("teste", ["html" => $res]);
         $numeros = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57);
         $ccg = array("PO", "PO", "3/4 HOL + 1/4 GIR", "5/8 HOL + 3/8 GIR", "5/8 HOL + 3/8 GIR", "5/8 HOL + 3/8 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "5/8 HOL + 3/8 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "5/8 HOL + 3/8 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/4 HOL + 3/4 GIR", "1/4 HOL + 3/4 GIR", "5/8 HOL + 3/8 GIR", "5/8 HOL + 3/8 GIR", "5/8 HOL + 3/8 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/2 HOL + 1/2 GIR", "1/4 HOL + 3/4 GIR", "5/8 HOL + 3/8 GIR", "5/8 HOL + 3/8 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR", "3/4 HOL + 1/4 GIR");
+        $reserva = Reserva::where("id", 42)->with("lotes")->get();
+        dd($reserva);
         for($i = 0; $i < count($numeros); $i++){
-            $reserva = Reserva::where("id", 42)->with("lotes")->get();
             $lote = $reserva->lotes->where("numero", $numeros[$i])->first();
             $lote->ccg = $ccg[$i];
             $lote->save();
