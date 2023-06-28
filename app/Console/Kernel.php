06b6cf4d176988e6d4a19b175524e8047c3fe878
Luis Gustavo de Souza Carvalho <gusouza980@gmail.com>
2023-06-28 15:57:58 -0300

    Update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,9 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('reserva:relatorio')->everyMinute();; // email diários
-        $schedule->command('notificacao:clientesReprovados')->everyMinute(); // email diários
-        // $schedule->command('inspire')->hourly();
+        // $schedule->command('notificacao:clientesReprovados')->everyMinute(); // email diários
     }
 
     /**
