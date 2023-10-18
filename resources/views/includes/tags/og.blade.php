67b2f46b17aef4c333ca2598201ce034b38683fa
Luis Gustavo de Souza Carvalho <gusouza980@gmail.com>
2023-10-18 12:51:21 -0300

    update

--- resources/views/includes/tags/og.blade.php
+++ resources/views/includes/tags/og.blade.php
@@ -4,7 +4,7 @@
 <meta property="og:url" content="{{ url()->full() }}" />    
 <meta property="og:site_name" content="Agroreserva @if(session()->get('nome_pagina')) - {{ session()->get('nome_pagina') }}  @endif" >
 <meta property="og:image" itemprop="image"
-    content="https://agroreserva.com.br/imagens/logo_agroreserva_leite.jpg">
+    content="https://agroreserva.com.br/imagens/relogio.jpg">
 <meta property="og:image:type" content="image/jpeg">
 {{-- <meta property="og:image:width" content="799">
 <meta property="og:image:height" content="294"> --}}
