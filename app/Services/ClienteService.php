2606a9fc67b374ea72258ebee2fc8b0348ec6675
Luis Gustavo de Souza Carvalho <gusouza980@gmail.com>
2023-12-12 07:45:45 -0300

    Update

--- app/Services/ClienteService.php
+++ app/Services/ClienteService.php
@@ -1,20 +1,32 @@
 <?php
 
 namespace App\Services;
+use Pedroni\RdStation\Facades\RdStation;
+
 
 class ClienteService
 {
 
     public function sendRdstation($cliente){
-        $rdStation = new \RDStation\RDStation($cliente->email);
-        $rdStation->setApiToken('ff3c1145b001a01c18bfa3028660b6c6');
-        $rdStation->setLeadData('name', $cliente->nome_dono);
-        $rdStation->setLeadData('identifier', 'cadastro');
-        $rdStation->setLeadData('telefone', $cliente->telefone);
-        $rdStation->setLeadData('rg', $cliente->rg);
-        $rdStation->setLeadData('cpf', $cliente->cpf);
-        $rdStation->setLeadData('nascimento', $cliente->nascimento);
-        $rdStation->setLeadData('estado_civil', config("clientes.estados_civis.$cliente->estado_civil") ?? '');
-        $rdStation->sendLead();
+        // $rdStation = new \RDStation\RDStation($cliente->email);
+        // $rdStation->setApiToken('ff3c1145b001a01c18bfa3028660b6c6');
+        // $rdStation->setLeadData('name', $cliente->nome_dono);
+        // $rdStation->setLeadData('identifier', 'cadastro');
+        // $rdStation->setLeadData('telefone', $cliente->telefone);
+        // $rdStation->setLeadData('rg', $cliente->rg);
+        // $rdStation->setLeadData('cpf', $cliente->cpf);
+        // $rdStation->setLeadData('nascimento', $cliente->nascimento);
+        // $rdStation->setLeadData('estado_civil', config("clientes.estados_civis.$cliente->estado_civil") ?? '');
+        // $rdStation->sendLead();
+        RdStation::events()->conversion([
+            'email' => $cliente->email,
+            'name' => $cliente->nome_dono,
+            'conversion_identifier' => 'cadastro',
+            'telefone' => $cliente->telefone,
+            'rg' => $cliente->rg,
+            'cpf' => $cliente->cpf,
+            'nascimento' => $cliente->nascimento,
+            'estado_civil' => config("clientes.estados_civis.$cliente->estado_civil") ?? '',
+        ]);
     }
 }
