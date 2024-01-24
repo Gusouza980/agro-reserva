e8c3346f971d4e630b9917365dcb84f5387046ac
Luis Gustavo de Souza Carvalho <gusouza980@gmail.com>
2024-01-24 13:19:39 -0300

    Update

--- resources/views/sistema/index.blade.php
+++ resources/views/sistema/index.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('conteudo')
-    @if($usuario->assessor)
+    @if(isset($usuario->assessor))
         <livewire:sistema.dashboards.comercial></livewire:sistema.dashboards.comercial>
     @endif
 @endsection
